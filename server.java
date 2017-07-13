99011e5f0e9e747ebf12106f203d621cdbcced9b
Blair Davis <davis.blairrosalyn@gmail.com>
2017-07-13 12:00:24 -0700

    added functionality to server

--- server.java
+++ server.java
@@ -9,9 +9,13 @@ public class server {
 
 	public static void main(String[] args) {
 		try {
+			//start server
 			server = new ServerSocket(1990);
+			System.out.println("Server is running");
+			
+			//accept client
 			client = server.accept();
-		
+			System.out.println("Client has connected to server");		
 			//obtain and print data from client
 			dataFromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
