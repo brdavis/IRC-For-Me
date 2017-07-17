ebbf710439ca17a3140dbc78194df54f1dbc6a02
Blair Davis <davis.blairrosalyn@gmail.com>
2017-07-16 19:23:33 -0700

    added ability for server to echo back to client

--- client.java
+++ client.java
@@ -1,15 +1,27 @@
+import java.util.*;
 import java.net.*;
 import java.io.*;
 
 public class client {
-	private static Socket connectionToServer;
-	private static PrintWriter content;
 
 	public static void main(String[] args) {
 		try {
-			connectionToServer = new Socket("localhost", 1990);
-			content = new PrintWriter(connectionToServer.getOutputStream(), true);
-			content.println("Testing 1, 2, 3");
+			//connecting to server
+			Socket connectionToServer = new Socket("localhost", 1990);
+
+			//get data from user
+			Scanner userInput = new Scanner(System.in);
+			System.out.println("Enter input");
+			String input= userInput.nextLine();
+
+			//send data to server
+			PrintStream p = new PrintStream(connectionToServer.getOutputStream());
+			p.println(input);
+
+			//receive data from server
+			Scanner recievedFromServer = new Scanner (connectionToServer.getInputStream());
+			String temp = recievedFromServer.nextLine();
+			System.out.println(temp);
 
 		} catch (Exception e) {
 			System.out.println(e);
--- server.java
+++ server.java
@@ -1,28 +1,29 @@
+import java.util.Scanner;
 import java.net.*;
 import java.io.*;
 
 public class server {
-	private static ServerSocket server;
-	private static Socket client;
-	private static BufferedReader dataFromClient;
-	private static String inputLine;
 
 	public static void main(String[] args) {
 		try {
 			//start server
-			server = new ServerSocket(1990);
+			ServerSocket server = new ServerSocket(1990);
 			System.out.println("Server is running");
 			
 			//accept client
-			client = server.accept();
+			Socket client = server.accept();
 			System.out.println("Client has connected to server");		
-			//obtain and print data from client
-			dataFromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		
+			//recieve data from client
+			Scanner clientData = new Scanner(client.getInputStream());
+			String temp;
+			temp = clientData.nextLine();
 
-			while ((inputLine = dataFromClient.readLine()) != null){
-				System.out.println(inputLine);
-			}
-		} catch(IOException e) {
+			//sent data to client
+			PrintStream p = new PrintStream(client.getOutputStream());
+			p.println(temp);
+
+			} catch(IOException e) {
 			System.out.println(e);
 		}
 	}
