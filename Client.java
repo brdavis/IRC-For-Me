778923f49605cd2b4953b566e2c4eff906987ef6
Blair Davis <davis.blairrosalyn@gmail.com>
2017-08-08 13:03:04 -0700

    Client-side IRC files

--- Client.java
+++ Client.java
@@ -1,40 +1,50 @@
-// This class starts a new client and connects it to the server
-// Its primary functionality is to start the client, client handler (to send messages), and recieve messages from the server
+/**
+* This class starts a new client and connects it to the IRC-For-Me server
+* Its primary functionality is to start the client, client handler (to send messages), and recieve messages from the server
+**/
 
 import java.io.*;
 import java.util.*;
 import java.net.*;
 
 public class Client {
+
 	public static final int ERROR = -1;
 	public static final String SERVER_HOSTNAME = "localhost";
 	public static final int SERVER_PORT = 1990;
 
 	public static void main(String[] args) {
+
 		BufferedReader server_output = null;
 		PrintWriter client_output = null;
-		//connecting client to server
+
+		/**
+		* Connecting client to IRC-For-Me server
+		**/
 		try {
 			//connect client to IRC-For-Me server
 			Socket socket = new Socket(SERVER_HOSTNAME, SERVER_PORT);
+			System.out.println("Connected to the server sucessfully");
 
 			//set up two-way communication channel between client and server
 			server_output = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			client_output = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-			System.out.println("Connected to the server sucessfully");
+
 		} catch (IOException er) {
 			System.err.println("Error: Cannot connect to server");
 			System.exit(ERROR);
 		}		
 
-		//Sending messages
-		//Create and start the Client_handler for sending messages
+		/**
+		* Create and start the Client_handler for sending and recieving messages
+		**/
 		Client_handler client_messenger = new Client_handler(client_output);
 		client_messenger.setDaemon(true);
 		client_messenger.start();
 		
-		//Recieving messages
-		//this will take messages from server and print them on the user console
+		/**
+		* Recieving messages from the server
+		**/
 		try {
 			String incoming_message;
 			while((incoming_message = server_output.readLine()) != null) {
--- Client_handler.java
+++ Client_handler.java
@@ -1,18 +1,29 @@
-// This class is a client-side event handler to handle all keyboad events from the client
-// There is one thread per client
-// This event handler will transmit all user data to the server
+/**
+* This class is a client-side event handler to handle all keyboad events from the client
+* This event handler will transmit all user data to the server
+**/
+
 import java.io.*;
 
 public class Client_handler extends Thread {
+	
+	/**
+	* Class variables
+	**/	
 	public static final int ERROR = -1;
 	private PrintWriter client_message;
 
+	/**
+	* Constructor method
+	**/
 	public Client_handler(PrintWriter message) {
 		this.client_message = message;
 	}
 
+	/**
+	* Standard run method to start thread functionality
+	**/
 	public void run() {
-	//sends messages form console to the server throught the socket
 		try {
 			BufferedReader client_input = new BufferedReader(new InputStreamReader(System.in));
 			while(!isInterrupted()) {
@@ -24,5 +35,5 @@ public class Client_handler extends Thread {
 			System.out.println("Error: Unable to transmit user data");
 			System.exit(ERROR);
 		}
-	}
+	} 
 }
